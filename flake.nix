64ca6edac7d028bafa290d99a546838f0118e6e4
Lova Andriarimalala <43842786+Xpirix@users.noreply.github.com>
2026-02-27 13:35:51 +0300

    Update nixpkgs version from 25.05 to 25.11

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   description = "Shared nixpkgs versions for QGIS";
-  inputs.nixpkgs-25-05.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs-25-11.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs =
     {
       self,
-      nixpkgs-25-05,
+      nixpkgs-25-11,
       nixpkgs-unstable,
     }:
     {
-      inherit nixpkgs-25-05 nixpkgs-unstable;
+      inherit nixpkgs-25-11 nixpkgs-unstable;
     };
 }
